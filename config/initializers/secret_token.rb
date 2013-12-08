e1094bc1c3dbc7818883e90099ac68e53215591f
jalemieux@gmail.com <jalemieux@gmail.com>
2013-12-08 11:52:34 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baserails::Application.config.secret_token = '76e6af961ba4208613fd68444e680dfff76cec4c50b097b2597f4a8d21eb0305146d1676161c91980b5c4ddb8caef7f64cc5eb7f2021dde9656b33c069da63bc'
